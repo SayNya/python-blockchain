95237e7d24bebf11e04932c616f01454de00641f
Alexey Lapitski <alexey.lapitski@intexsoft.by>
2022-06-27 12:25:27 +0300

    added poetry and makefile

---
+++ Makefile
@@ -0,0 +1,4 @@
+include Makefile.variable
+
+build-local:
+	/bin/bash ${build_path} ${python_version_path}
--- src/main.py
+++ src/main.py
@@ -4,5 +4,4 @@ G = S256Point(
     0x79be667ef9dcbbac55a06295ce870b07029bfcdb2dce28d959f2815b16f81798,
     0x483ada7726a3c4655da4fbfc0e1108a8fd17b448a68554199c47d08ffb10d4b8
 )
-print(G)
-print(type(G + G))
+print(0x79be667ef9dcbbac55a06295ce870b07029bfcdb2dce28d959f2815b16f81798 * G)
